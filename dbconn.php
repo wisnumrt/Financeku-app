0a1e0b2ca1e4797425c8c0e0809deb55e847ccfb
wisnumrt <wisnumurti982@gmail.com>
2025-05-05 23:37:56 +0700

    Update konfigurasi database

--- dbconn.php
+++ dbconn.php
@@ -2,10 +2,10 @@
 function getConnection()
 {
     // membuat koneksi ke database system
-    $dbServer = 'sql300.infinityfree.com';
-    $dbUser = 'if0_38901134';
-    $dbPass = '0506Sandi';
-    $dbName = "if0_38901134_if0_financeku2";
+    $dbServer = 'localhost';
+    $dbUser = 'root';
+    $dbPass = '';
+    $dbName = "dbfinanceku";;
 
     // membuat koneksi
     $conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName);
